6501206c288451d92f769a201fba6b1d4e0d5c8f
soo-ryu <ryusoohyun12@gmail.com>
2025-04-06 00:28:14 +0200

    Make public folder

--- main.js
+++ main.js
@@ -1,8 +1,8 @@
 import * as THREE from 'three';
-import './style/style.css';
-import gsap from "gsap";
-import {OrbitControls} from 'three/examples/jsm/controls/OrbitControls';
-import { OBJLoader } from 'three/examples/jsm/loaders/OBJLoader.js';
+import '/style/style.css';
+import gsap from "node-modules/gsap";
+import {OrbitControls} from 'node-modules/three/examples/jsm/controls/OrbitControls';
+import { OBJLoader } from 'node-modules/three/examples/jsm/loaders/OBJLoader.js';
 
 // Texture Loader
 //const loader = new THREE.TextureLoader()
@@ -48,7 +48,7 @@ scene.add(mesh, particlesMesh)
 // Load Model
 const objloader = new OBJLoader();
 objloader.load(
-  './assets/tree.obj', 
+  '/assets/tree.obj', 
   function ( object ) {
     scene.add( object );
   },
