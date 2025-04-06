4211a92548c9ddc4b14d760670c8dd5d04bd4e16
soo-ryu <ryusoohyun12@gmail.com>
2025-04-06 08:57:13 +0200

    change main.js

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 import * as THREE from 'three';
-import '/style/style.css';
+import 'public/style/style.css';
 import gsap from "gsap";
 import {OrbitControls} from 'three/examples/jsm/controls/OrbitControls';
 import { OBJLoader } from 'three/examples/jsm/loaders/OBJLoader.js';
@@ -48,7 +48,7 @@ scene.add(mesh, particlesMesh)
 // Load Model
 const objloader = new OBJLoader();
 objloader.load(
-  '/assets/tree.obj', 
+  'public/assets/tree.obj', 
   function ( object ) {
     scene.add( object );
   },
